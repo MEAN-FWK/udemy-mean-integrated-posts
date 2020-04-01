93aa3bc59d0edd1b8b1cbabcc1f81a8496a92bb0
Fidel Santana <in3pi287@gmail.com>
2020-04-01 19:11:20 -0400

    Fetching Data From a Database

--- backend/app.js
+++ backend/app.js
@@ -44,22 +44,12 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "asdf4534523",
-      title: "First server-side post",
-      content: "This is coming from the server "
-    },
-    {
-      id: "tytryr67567",
-      title: "Second server-side post",
-      content: "This is coming from the server "
-    }
-  ];
-  return res.status(200).json({
-    message: "Posts fetched succesfully",
-    posts: posts
-  });
+  return Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched succesfully",
+      posts: documents
+    });
+  })
 });
 
 module.exports = app;
