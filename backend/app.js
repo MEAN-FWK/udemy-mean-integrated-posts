545f5ebc6886533d773fc5836ae2e5687ea2356b
Fidel Santana <in3pi287@gmail.com>
2020-04-01 16:42:05 -0400

    Creating a POST Instance

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+
+const Post = require('./models/post');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +21,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: "Post added successfully"
