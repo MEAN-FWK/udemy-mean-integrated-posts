220877d15085e8d843c0c25cea515138d7543ce4
Fidel Santana <in3pi287@gmail.com>
2020-04-02 14:49:05 -0400

    Updating Posts on the Server

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,25 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/api/posts", (req, res, next) => {
+  return Post.find().then(documents => {
+    res.status(200).json({
+      message: "Posts fetched succesfully",
+      posts: documents
+    });
+  });
+});
+
+app.get("/api/posts/:id", (req, res, next) => {
+  return Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not found" });
+    }
+  });
+});
+
 app.post("/api/posts", (req, res, next) => {
   const post = new Post({
     title: req.body.title,
@@ -45,15 +64,6 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
-app.get("/api/posts", (req, res, next) => {
-  return Post.find().then(documents => {
-    res.status(200).json({
-      message: "Posts fetched succesfully",
-      posts: documents
-    });
-  });
-});
-
 app.put("/api/posts/:id", (req, res, next) => {
   const post = new Post({
     _id: req.params.id,
@@ -61,7 +71,6 @@ app.put("/api/posts/:id", (req, res, next) => {
     content: req.body.content
   });
   Post.updateOne({ _id: req.params.id }, post).then(result => {
-    console.log(result);
     res.status(200).json({
       message: "Update succesfully"
     });
