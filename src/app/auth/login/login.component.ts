d2149dbe5dd4008e3fe24c0717e6199a3416e536
Fidel Santana <in3pi287@gmail.com>
2020-04-05 12:25:53 -0400

    Sending the Token to the Frontend

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +9,16 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
   isLoading = false;
 
   ngOnInit() {
   }
 
   onLogin(form: NgForm) {
-    console.log(form.value);
+    if (form.valid) {
+      this.authService.login(form.value.email, form.value.password);
+    }
   }
 
 }
