c7a2b9d8ed5cad20d3666d440de008def9a5c113
Fidel Santana <in3pi287@gmail.com>
2020-04-06 15:42:27 -0400

    Creating the Auth Module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { AngularMaterialModule } from '../angular-material.module';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    RouterModule
+  ]
+})
+export class AuthModule { }
