7dab552974a75ef6ff8819ce8d033fec5c1fe53b
Fidel Santana <in3pi287@gmail.com>
2020-04-05 23:28:23 -0400

    Displaying the Basic Error Dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -20,37 +21,41 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostCreateComponent,
-    HeaderComponent,
-    PostListComponent,
-    LoginComponent,
-    SignupComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    HttpClientModule
-  ],
+   declarations: [
+      AppComponent,
+      PostCreateComponent,
+      HeaderComponent,
+      PostListComponent,
+      LoginComponent,
+      SignupComponent,
+      ErrorComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      ReactiveFormsModule,
+      FormsModule,
+      BrowserAnimationsModule,
+      MatInputModule,
+      MatCardModule,
+      MatButtonModule,
+      MatToolbarModule,
+      MatExpansionModule,
+      MatProgressSpinnerModule,
+      MatPaginatorModule,
+      MatDialogModule,
+      HttpClientModule
+   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
   ],
-  bootstrap: [
-    AppComponent
-  ]
+   bootstrap: [
+      AppComponent
+  ],
+   entryComponents: [ErrorComponent]
 })
 export class AppModule { }
